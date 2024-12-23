b056a9f1343d254a05aae95ba81e84521161422f
Luis Henrique <luishng@gmail.com>
2024-12-23 12:34:09 -0300

    fix: E2E testing - 3

--- test/order.e2e-spec.ts
+++ test/order.e2e-spec.ts
@@ -64,7 +64,7 @@ test('filter by status', async ({ page }) => {
   await page.getByLabel('Pendente').click()
   await page.getByRole('button', { name: 'Filtrar resultados' }).click()
 
-  const tableRows = await page.getByRole('cell', { name: 'Pendente' }).all()
+  const tableRows = page.getByRole('cell', { name: 'Pendente' })
 
-  await expect(tableRows).toHaveLength(10)
+  await expect(tableRows).toHaveCount(10)
 })
